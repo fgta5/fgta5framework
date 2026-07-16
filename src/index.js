1b451f30b2ac86eee2054184df73e4d0b76e8f64
agungdhewe <agung_dhewe@yahoo.com>
2026-07-16 20:04:24 +0700

    update variable APPTITLE

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const webapp = createWebApplication()
 const appName = process.env.APPNAME
-const appTitle = process.env.APPTILE
+const appTitle = process.env.APPTITLE
 
 
 main()
